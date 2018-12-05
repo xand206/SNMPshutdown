c21d169521142b780c79d42a1e53f214af83ccbf
xand206 <4418209+xand206@users.noreply.github.com>
2018-12-05 10:33:40 -0200

    Update SNMPshutdown.sh
    Definido variável do arquivo de log.
    Definido variável para padrão de data.
    Criada ação para registro de log

--- SNMPshutdown.sh
+++ SNMPshutdown.sh
@@ -5,10 +5,15 @@ ACIN_OID="1.3.6.1.4.1.318.1.1.1.3.2.1.0"                #OID - AC na entrada
 TIME_OID="1.3.6.1.4.1.318.1.1.1.2.2.3.0"                #OID - Tempo estimado em modo de Bateria
 ACIN_LOW=70                                             #(Volt) Valor mínimo aceitável na entrada AC - Ver datasheet
 TIME_LOW=180000                                         #(Centisegundo) Valor mínimo aceitável do tempo restante em modo de bateria
+LOG="/var/log/snmpshutdown.log"                         #Arquivo de log
+DATE=$(date '+%Y-%m-%d %H:%M:%S')                       #Padrão de Data
 
 #INICIO
 ACIN_READ=$(snmpget -v2c -c $COMMUNITY $NOBREAKIP $ACIN_OID | awk '{ print $4 }')               #Valor lido no SNMP
 TIME_READ=$(snmpget -v2c -c $COMMUNITY $NOBREAKIP $TIME_OID | tr -d "()" | awk '{ print $4 }')  #Valor lido no SNMP
+
+#LOG
+echo $DATE "Input Voltage:" $ACIN_READ "Runtime Remaining:" $(bc <<< "$TIME_READ / 6000") "min"  >> $LOG
 
 if [ -n "$ACIN_READ" ] && [ -n "$TIME_READ" ]                                   #Verifica se as variáveis são nulas 
 then
